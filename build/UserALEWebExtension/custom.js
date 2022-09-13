4d2c35c7d6404d88ef20929fc6300ae479d56567
Amir Ghaemi <51731855+amirmghaemi@users.noreply.github.com>
2022-09-13 11:50:06 -0400

    minor change to prevent some browser-side errors

--- build/UserALEWebExtension/custom.js
+++ build/UserALEWebExtension/custom.js
@@ -1040,7 +1040,7 @@ window.onload = function () {
   endLoadTimestamp = Date.now();
 };
 
-window.userale.filter(function (log) {
+browser.userale.filter(function (log) {
   var type_array = ['mouseup', 'mouseover', 'mousedown', 'keydown', 'dblclick', 'blur', 'focus', 'input', 'wheel'];
   var logType_array = ['interval'];
   return !type_array.includes(log.type) && !logType_array.includes(log.logType);
