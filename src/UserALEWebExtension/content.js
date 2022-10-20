462e4b76846732d12fad1a53d462b5a7804339b6
Amir Ghaemi <51731855+amirmghaemi@users.noreply.github.com>
2022-10-20 12:00:42 -0400

    added the custom filter - keeping documentation

--- src/UserALEWebExtension/content.js
+++ src/UserALEWebExtension/content.js
@@ -82,5 +82,9 @@ browser.runtime.onMessage.addListener(function (message) {
 //Add additional custom scripts below this line
 //=============================================
 
-
+filter(function (log) {
+  var type_array = ['mouseup', 'mouseover', 'mousedown', 'keydown', 'dblclick', 'blur', 'focus', 'input', 'wheel'];
+  var logType_array = ['interval'];
+  return !type_array.includes(log.type) && !logType_array.includes(log.logType);
+});
 
